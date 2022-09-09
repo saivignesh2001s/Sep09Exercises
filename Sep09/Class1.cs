61015a468273b678c6457571f8ed2a3b3deae802
saivignesh2001s <saivignesh2001s@gmail.com>
2022-09-09 14:53:40 +0530

    Add project files.

---
+++ Sep09/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sep09
+{
+    public class Class1
+    {
+        public static void Ma()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i*2);
+            }
+        }
+
+    }
+}
---
+++ Sep09Exercises/Program.cs
@@ -0,0 +1,31 @@
+﻿using Sep09;
+namespace Sep09Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Without using thread start");
+            Thread t1 = new Thread(Ma);
+            
+            t1.Start();
+
+            
+            Console.WriteLine("using parameterised thread start");
+            Thread t3 = new Thread(new ThreadStart(Ma));
+            t3.Start();
+            Console.ReadLine();
+        }
+        public static void Ma()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 3)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
