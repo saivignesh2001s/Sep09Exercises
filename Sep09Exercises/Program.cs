66a313d15e800505c1a26933e8f9b5dc0863d964
saivignesh2001s <saivignesh2001s@gmail.com>
2022-09-09 21:09:38 +0530

    dd

--- Sep09Exercises/Program.cs
+++ Sep09Exercises/Program.cs
@@ -1,29 +1,41 @@
 ﻿using Sep09;
 namespace Sep09Exercises
 {
-    internal class Program
+    class MutexWithReleaseDemo
     {
+        private static Mutex mutex = new Mutex();
+        const int limits = 1;
+        const int threads = 4;
+
+        static void ThreadsDemo()
+        {
+            for (int i = 0; i < limits; i++)
+            {
+                UseCode();
+            }
+        }
+        static void UseCode()
+        {
+            mutex.WaitOne();   // Wait until it is safe to enter.  
+            Console.WriteLine("{0} has entered in the code",
+                Thread.CurrentThread.Name);
+            // Place code to access non-reentrant resources here.  
+            Thread.Sleep(500);    // Wait until it is safe to enter.  
+            Console.WriteLine("{0} is leaving the code\r\n",
+                Thread.CurrentThread.Name);
+            mutex.ReleaseMutex();    // Release the Mutex.  
+
+
+        }
         static void Main(string[] args)
         {
-            using (Mutex mutex = new Mutex(false, "Sep09Exercises"))
+            for (int i = 0; i < threads; i++)
             {
-
-                if (!mutex.WaitOne(5000,false))
-                {
-                    Monitor.Enter(mutex);
-                    Console.WriteLine("already running....");
-                    Console.ReadLine();
-                    return;
-
-                }
-                else
-                {
-                    Console.WriteLine("app is running");
-                    Console.ReadLine();
-                    Monitor.Exit(0);
-                }
-
+                Thread t = new Thread(new ThreadStart(ThreadsDemo));
+                t.Name = String.Format("Thread{0}", i + 1);
+                t.Start();
             }
+            Console.Read();
 
         }
     }
